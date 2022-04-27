a0a55f70fb037dab6a8406e5d50ec938b6d97228
Wizock <36351054+Wizock@users.noreply.github.com>
2022-04-28 06:31:13 +1000

    .

--- nuclei/compression_service/views.py
+++ nuclei/compression_service/views.py
@@ -1,8 +1,6 @@
-import pathlib
-
+import pathlib, os, shutil, base64, hashlib
 from flask import Blueprint, Flask, jsonify, render_template, request, url_for
 from nturl2path import url2pathname
-
 from .models import CompressionService, db
 from werkzeug.utils import secure_filename
 
@@ -10,41 +8,52 @@ compression_service_blueprint = Blueprint(
     "compression_service",
     __name__,
     template_folder="templates",
-    static_folder="file_storage",
+    static_folder="/file_storage/",
 )
 
 
 @compression_service_blueprint.route("/index")
 def index():
-    return "Hello, world!"
-
-
-@compression_service_blueprint.route("/compress/<string:file_path>")
-def compress(file_path):
-    return "Compressing file: {}".format(file_path)
-
-
-@compression_service_blueprint.route("/decompress/<string:file_path>")
-def decompress(file_path):
-    return "Decompressing file: {}".format(file_path)
-
+    # query all compression services
+    compression_services = CompressionService.query.all()
+    return render_template("indexer.html", compression_services=compression_services)
 
 # file upload endpoint
 @compression_service_blueprint.route("/upload", methods=["POST", "GET"])
 def upload():
     if request.method == "POST":
-        # get the file from the request
         file = request.files["file"]
-        print(file)
-        # get the file name
+
         file_name = secure_filename(file.filename)
-        print(file_name)
-        # get pathlib to get the path to file_storage
         file_storage_path = str(pathlib.Path.cwd()) + str(pathlib.Path(r"\nuclei\compression_service\file_storage"))+str(rf"\{file_name}")
-        print(file_storage_path)
-        # save the file
         file.save(file_storage_path)
-        # return the file path
+
+        # get file size
+        file_size = os.path.getsize(file_storage_path)
+        # get file hash
+        file_hash_md5 = hashlib.md5(open(file_storage_path, "rb").read()).hexdigest()
+        # get file base64
+        file_base64 = base64.b64encode(open(file_storage_path, "rb").read()).decode("utf-8")
+        # get file extension
+        file_extension = os.path.splitext(file_storage_path)[1]
+        # get file path
+        file_path = os.path.dirname(file_storage_path)
+
+        # create new CompressionService object
+        compression_service = CompressionService(
+            name=file_name,
+            file_path=file_path,
+            file_name=file_name,
+            file_extension=file_extension,
+            file_size=file_size,
+            file_hash_md5=file_hash_md5,
+            file_base64=file_base64,
+        )
+        # add new CompressionService object to database
+        db.session.add(compression_service)
+        # commit changes to database
+        db.session.commit()
+
         return file_storage_path
     else:
         return """
