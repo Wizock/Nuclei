69802c0e36da197cf6271014396f3f8e6d20a8dd
Wizock <silent.death3500@gmail.com>
2022-05-05 04:22:37 +1000

    .

--- nuclei/__init__.py
+++ nuclei/__init__.py
@@ -2,12 +2,12 @@
 import secrets
 from secrets import token_urlsafe
 
+import flask_security
 from flask import Flask
 from flask_admin import Admin
 from flask_caching import Cache
 from flask_cors import CORS
 from flask_debugtoolbar import DebugToolbarExtension
-
 # from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -21,18 +21,42 @@ from nuclei.database import import_tables
 class Nuclei(Flask):
     def __init__(self, import_name, template_folder=None, root_path=None):
         super().__init__(import_name, template_folder, root_path)
-        self.config["SECRET_KEY"] = secrets.token_hex(16)
-        self.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/nuclei.db"
-        self.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        self.config["SQLALCHEMY_ECHO"] = False
-        self.config["MAIL_SERVER"] = "smtp.gmail.com"
-        self.config["MAIL_PORT"] = 465
-        self.config["MAIL_USE_SSL"] = True
-
         self.db = SQLAlchemy(self)
+
+    def return_db(self) -> SQLAlchemy:
+        return self.db
+
+
+
+
+class Database_Register(object):
+    def __init__(self, app: Nuclei):
+        self.app = app
+
+    def register_database(self):
+        import_tables(self.app)
+
+class Config_Register(object):
+    def __init__(self, app: Nuclei):
+        self.app = app
+        self.app.config["SECRET_KEY"] = secrets.token_hex(16)
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/nuclei.db"
+        self.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        self.app.config["DEBUG"] = True
+        self.app.config["CACHE_TYPE"] = "simple"
+        self.app.config["CACHE_DEFAULT_TIMEOUT"] = 300
+        self.app.config["SQLALCHEMY_ECHO"] = False
+        self.app.config["SQLALCHEMY_RECORD_QUERIES"] = False
+        self.app.config["SQLALCHEMY_BIND_DECLARED_SOURCES"] = True
+        self.app.config["MAIL_SERVER"] = "smtp.gmail.com"
+        self.app.config["MAIL_PORT"] = 465
+        self.app.config["MAIL_USE_SSL"] = True
+
+
+class extensions(object):
+    def __init__(self, app: Nuclei):
+        self.app = app
         self.migrate = Migrate(self, self.db)
-        # self.login_manager = LoginManager(self)
-        # self.login_manager.login_view = "auth.login"
         self.mail = Mail(self)
         self.admin = Admin(self, name="Nuclei")
         self.cors = CORS(self, resources={r"/*": {"origins": "*"}})
@@ -40,24 +64,9 @@ class Nuclei(Flask):
         self.cache = Cache(self)
         self.debugtoolbar = DebugToolbarExtension(self)
 
-        self.register_extensions()
-
-    def return_db(self) -> SQLAlchemy:
-        return self.db
-
-    def register_blueprints(self):
-        from nuclei.admin_interface.views import admin_interface_blueprint
-        from nuclei.authentication.views import authentication_blueprint
-        from nuclei.compression_service.views import compression_service_blueprint
-
-        self.register_blueprint(compression_service_blueprint)
-        self.register_blueprint(admin_interface_blueprint)
-        self.register_blueprint(authentication_blueprint)
-
     def register_extensions(self):
         self.db.init_app(self)
         self.migrate.init_app(self)
-        # self.login_manager.init_app(self)
         self.mail.init_app(self)
         self.cors.init_app(self)
         self.socketio.init_app(self)
@@ -65,8 +74,12 @@ class Nuclei(Flask):
         self.debugtoolbar.init_app(self)
 
 
-app = Nuclei(__name__)
+__app__ = Nuclei(__name__)
+Configurations = Configs()
+extensions = extensions()
 
-with app.app_context():
-    import_tables(app)
-    app.register_blueprints()
+with __app__.app_context():
+    import_tables(__app__)
+    __app__.register_blueprints()
+    __app__.config.from_object(Configurations)
+    extensions().register_extensions()
---
+++ nuclei/blueprint_register.py
@@ -0,0 +1,14 @@
+
+class Blueprints_Register(object):
+    def __init__(self, app: Nuclei):
+        self.app = app
+
+    def register_blueprints(self):
+        from nuclei.admin_interface.views import admin_interface_blueprint
+        from nuclei.authentication.views import authentication_blueprint
+        from nuclei.compression_service.views import \
+            compression_service_blueprint
+
+        self.app.register_blueprint(compression_service_blueprint)
+        self.app.register_blueprint(admin_interface_blueprint)
+        self.app.register_blueprint(authentication_blueprint)
