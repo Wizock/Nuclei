5b0f34c4fec54d3d77508091a48fd2731e772a93
Wizock <silent.death3500@gmail.com>
2022-05-23 19:52:26 +1000

    gpu doesnt work...

--- nuclei/compression_service/video_compression/compression_preset.py
+++ nuclei/compression_service/video_compression/compression_preset.py
@@ -1,4 +1,5 @@
 import subprocess
+from numba import jit, cuda
 
 """
 transform these functions into generators
@@ -12,34 +13,33 @@ else, the process is not running
 
 """
 
-
+@jit(target ="cuda") 
 def compression_low_preset(file_path, file_path_compressed):
     return subprocess.run(
         f"ffmpeg -i {file_path} -vcodec libx264 -crf 24 {file_path_compressed}",
         shell=True,
     )
 
-
+@jit(target ="cuda") 
 def compress_medium_preset(file_path, file_path_compressed):
     return subprocess.run(
         f"ffmpeg -i {file_path} -vcodec libx264 -crf 28 {file_path_compressed}",
         shell=True,
     )
 
-
+@jit(target ="cuda") 
 def compression_high_preset(file_path, file_path_compressed):
     return subprocess.run(
         f"ffmpeg -i {file_path} -vcodec libx264 -crf 30 {file_path_compressed}",
         shell=True,
     )
 
-
+@jit(target ="cuda") 
 def tested_perfect_preset(file_path, file_path_compressed):
     return subprocess.Popen(
         f"ffmpeg -i {file_path} -vcodec libx264 -crf 30 {file_path_compressed}",
         shell=True,
     ).wait()
-
 
 def compression_main(file_path, file_path_compressed, preset=None):
     if preset == "low":
